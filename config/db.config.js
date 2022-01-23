60938985d546fd3dbb371b65af02bdf64576295d
LinaBahrini <bahrini10@gmail.com>
2022-01-23 20:54:34 +0100

    a

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "postgres://lzxesvtokgbyls:f0979147d8a9304da8472fe7b1b35f5de22d092a489fa85e51e013daee6d560e@ec2-63-34-223-144.eu-west-1.compute.amazonaws.com:5432/deb50m765lm1iu",
+  HOST: "ec2-63-34-223-144.eu-west-1.compute.amazonaws.com",
   USER: "lzxesvtokgbyls",
   PASSWORD: "f0979147d8a9304da8472fe7b1b35f5de22d092a489fa85e51e013daee6d560e",
   DB: "deb50m765lm1iu",
